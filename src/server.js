1809ff443ab70c10722fec1b9a614dfdbaa057fb
Anna Anishchenko <annuwkaanishchenko@gmail.com>
2026-03-13 19:44:10 +0200

    01-express1

---
+++ src/server.js
@@ -0,0 +1,60 @@
+// src/server.js
+
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import 'dotenv/config';
+
+const app = express();
+
+// Використовуємо значення з .env або дефолтний порт 3000
+const PORT = process.env.PORT ?? 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(pino());
+
+// маршрут, який буде повертати всі нотатки:
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+// маршрут, який буде повертати одну нотатку за її ідентифікатором:
+app.get('/notes/:noteId', (req, res) => {
+  const id_param = Number(req.params);
+  res.status(200).json({
+    message: `Retrieved note with ID: ${id_param}`,
+  });
+});
+
+// Маршрут для тестування middleware помилки
+app.get('/test-error', (req, res) => {
+  // Штучна помилка для прикладу
+  throw new Error('Simulated server error');
+});
+
+// 404 middleware
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+// error middleware
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+  });
+});
+
+// Запуск сервера
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// GET-запит до маршруту "/health"
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'Ok!',
+  });
+});
